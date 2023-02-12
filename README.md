ce2d40075fd187b7599b71ebe5662f997fa4a13d
anton.pogrebnjak <anton.pogrebnjak@uni-konstanz.de>
2023-02-12 22:47:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bluetooth Locker
+A little bash script to connect to a bluetooth device and log the user out if the connection to the bluetooth device, hopefully only logging them out, if they walk away from their computer.
+
+## Usage
+```./bluelock.sh <bluetooth address> <rssi threshold>```
---
+++ bluelock.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+bt_addr=$1 # <your bluetooth device address>
+bt_threshold=$2 # <your threshold value>
+locked=false
+
+if [ -z $bt_addr ]; then
+    echo "Please provide bluetooth device address"
+    exit 1
+fi
+
+if [ -z $bt_threshold ]; then
+    echo "Please provide rssi threshold value"
+    exit 1
+fi
+
+while true; do
+    res=$(hcitool rssi $bt_addr)
+    rssi=$(echo $res | sed -r 's/(.*([0-9]+))/\2/g')
+
+    if [ -z $rssi ] && [ $locked != true ]; then
+        echo Device not connected!
+    else
+        echo RSSI is $rssi
+        if [ $rssi -gt $bt_threshold ] && [ $locked != true ]; then
+            locked=true
+            echo Locking screen... $locked
+            $(xdg-screensaver lock)
+        elif [ $rssi -lt $bt_threshold ] && [ $locked == true ]; then
+            locked=false
+            echo Unlocking screen... $locked
+            $(loginctl unlock-session)
+        fi
+    fi
+
+    sleep 1
+done
